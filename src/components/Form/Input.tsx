3349288a0069bcdef22fdb6c82ef928d217a5f59
Lucid Themes <lucidtemplates@gmail.com>
2025-11-23 16:22:53 +0000

    refactor(form): convert input to TS

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,61 @@
+interface InputProps {
+  type: string;
+  name: string;
+  value: string;
+  onChange?: (e: object) => void;
+  placeholder?: string;
+  required?: boolean;
+  autoComplete?: string;
+  className?: string;
+  maxLength?: number;
+  label?: string;
+  labelSrOnly?: boolean;
+  error?: string;
+}
+
+export default function Input({
+  type = 'text',
+  name,
+  value,
+  onChange,
+  placeholder,
+  required = false,
+  autoComplete = 'on',
+  className = '',
+  maxLength,
+  label,
+  labelSrOnly = true,
+  error,
+  ...props
+}: InputProps) {
+  const labelSrOnlyClass = labelSrOnly ? 'sr-only' : '';
+
+  return (
+    <div className="flex w-full flex-col gap-y-2.5">
+      {label && (
+        <label htmlFor={name} className={`block text-lg/8 text-boulder ${labelSrOnlyClass}`}>
+          {label}
+        </label>
+      )}
+      <input
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+        autoComplete={autoComplete}
+        className={`h-12.5 w-full rounded-sm border-1 bg-white px-5 text-lg/8 text-boulder transition-colors duration-300 ease-in-out outline-none placeholder:text-boulder hover:border-boulder focus:border-boulder ${className} ${error ? 'border-red-500' : 'border-pearl-bush'}`}
+        maxLength={maxLength}
+        {...props} // any additional props
+      />
+
+      {error && (
+        <span role="alert" className="text-lg text-red-500">
+          {error}
+        </span>
+      )}
+    </div>
+  );
+}
