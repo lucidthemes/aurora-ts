7813cd520952fb7e8e15d458164c804558fd36c4
lucidthemes <lucidtemplates@gmail.com>
2025-11-24 13:06:55 +0000

    refactor(footer): convert footer to TS (#12)
    * refactor(footer): convert footer to TS
    
    * refactor(footer): convert footer main to TS
    
    * refactor(footer): convert footer instagram to TS
    
    * refactor(footer): convert footer bottom to TS
    
    * refactor(footer): update changelog version text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,11 @@ import FooterInstagram from './FooterInstagram';
 import FooterMain from './FooterMain';
 import FooterBottom from './FooterBottom';
 
-export default function Footer({ mobileMenuClasses }) {
+interface FooterProps {
+  mobileMenuClasses: string;
+}
+
+export default function Footer({ mobileMenuClasses }: FooterProps) {
   return (
     <footer className={`relative ${mobileMenuClasses} transition-[right] duration-300 ease-in-out`}>
       <FooterInstagram />
