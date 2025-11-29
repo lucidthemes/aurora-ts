7e4cb9946cbc33fa95a6b76bbebec212b5c9572a
Lucid Themes <lucidtemplates@gmail.com>
2025-11-29 16:38:40 +0000

    fix(schemas): incorrect type of boolean on replyto field

--- src/schemas/posts/comment.schema.ts
+++ src/schemas/posts/comment.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const CommentSchema = z.object({
   id: z.number().int().positive(),
   postId: z.number().int().positive(),
-  replyTo: z.boolean().optional(),
+  replyTo: z.number().nullable().optional(),
   author: z.string(),
   avatar: z.string().optional(),
   datetime: z.string(),
