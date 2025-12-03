95fd0514b1e1575bd83d789c2843c8ded9c6bb38
Lucid Themes <lucidtemplates@gmail.com>
2025-12-03 12:18:58 +0000

    fix(cart): useCouponForm hook being called conditionally

--- src/features/cart/components/totals/CouponForm.jsx
+++ src/features/cart/components/totals/CouponForm.jsx
@@ -3,9 +3,9 @@ import Input from '@components/Form/Input';
 import Button from '@components/UI/Button';
 
 export default function CouponForm({ couponFormShow, cartCoupons, addCartCoupon }) {
-  if (!couponFormShow) return null;
-
   const { couponFormCoupon, couponFormError, handleFormChange, handleFormSubmit } = useCouponForm(cartCoupons, addCartCoupon);
+
+  if (!couponFormShow) return null;
 
   return (
     <form onSubmit={handleFormSubmit} className="flex flex-col gap-y-5" aria-label="Add coupon" noValidate>
