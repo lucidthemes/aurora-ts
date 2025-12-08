efec542b15248cb95ddd801b9f07c59aed502201
Lucid Themes <lucidtemplates@gmail.com>
2025-12-08 15:29:59 +0000

    refactor(utils): split create change event into input and textarea

--- src/utils/tests/events.ts
+++ src/utils/tests/events.ts
@@ -6,6 +6,18 @@ export function createChangeEvent(name: string, value: string): ChangeEvent<HTML
   } as unknown as ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
 }
 
+export function createInputChangeEvent(name: string, value: string): ChangeEvent<HTMLInputElement> {
+  return {
+    target: { name, value },
+  } as unknown as ChangeEvent<HTMLInputElement>;
+}
+
+export function createTextAreaChangeEvent(name: string, value: string): ChangeEvent<HTMLTextAreaElement> {
+  return {
+    target: { name, value },
+  } as unknown as ChangeEvent<HTMLTextAreaElement>;
+}
+
 export function createFormSubmitEvent(): FormEvent<HTMLFormElement> {
   return { preventDefault: () => {} } as unknown as FormEvent<HTMLFormElement>;
 }
