bdd3e77651ad00094a1901ca656be4fe53fae1d2
Lucid Themes <lucidtemplates@gmail.com>
2025-12-23 16:54:29 +0000

    refactor(promoBox): convert component to TS

--- src/features/home/promoBox/PromoBox.tsx
+++ src/features/home/promoBox/PromoBox.tsx
@@ -1,6 +1,16 @@
+import type { ElementType } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function PromoBox({ image, heading, headingLevel = 3, subHeading, link, position = 'bottom' }) {
+interface PromoBoxProps {
+  image?: string;
+  heading?: string;
+  headingLevel?: '1' | '2' | '3' | '4' | '5' | '6';
+  subHeading?: string;
+  link?: string;
+  position?: 'bottom' | 'top' | 'center';
+}
+
+export default function PromoBox({ image, heading, headingLevel = '3', subHeading, link, position = 'bottom' }: PromoBoxProps) {
   const defaultClasses = 'flex flex-col gap-y-1 bg-white py-2.5 px-5';
 
   const layoutClasses =
@@ -12,15 +22,17 @@ export default function PromoBox({ image, heading, headingLevel = 3, subHeading,
           ? 'left-1/2 absolute top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md'
           : 'w-full bottom-0 left-0';
 
-  const HeadingTag = `h${headingLevel || 3}`;
+  const HeadingTag = `h${headingLevel || 3}` as ElementType;
 
-  return (
-    <Link to={link} className="relative overflow-hidden rounded-md">
+  const content = (
+    <div className="relative overflow-hidden rounded-md">
       <img src={image} alt={heading} className="rounded-md" />
       <div className={`${defaultClasses} ${layoutClasses}`}>
         {heading && <HeadingTag>{heading}</HeadingTag>}
         {subHeading && <p>{subHeading}</p>}
       </div>
-    </Link>
+    </div>
   );
+
+  return link ? <Link to={link}>{content}</Link> : content;
 }
