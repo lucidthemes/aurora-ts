06ec71dbbcd64599a790bec26f8d22150f997074
Lucid Themes <lucidtemplates@gmail.com>
2025-12-23 17:33:57 +0000

    refactor(slideshow): convert hook to TS

---
+++ src/features/home/slideshow/useSlideshow.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+
+export default function useSlideshow(loop?: boolean, autoplay?: boolean) {
+  const autoplayOptions =
+    autoplay === true
+      ? Autoplay({ delay: 5000, stopOnMouseEnter: true, stopOnInteraction: false, rootNode: (emblaRoot: HTMLElement) => emblaRoot.parentElement })
+      : Autoplay({ playOnInit: false, stopOnInteraction: true });
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: loop, align: 'start' }, [autoplayOptions]);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
+  const scrollNext = useCallback(() => emblaApi && emblaApi.scrollNext(), [emblaApi]);
+  const scrollTo = useCallback((index: number) => emblaApi && emblaApi.scrollTo(index), [emblaApi]);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, [emblaApi, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onSelect();
+    setScrollSnaps(emblaApi.scrollSnapList());
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, setScrollSnaps, onSelect]);
+
+  return { emblaRef, scrollSnaps, selectedIndex, scrollPrev, scrollNext, scrollTo };
+}
